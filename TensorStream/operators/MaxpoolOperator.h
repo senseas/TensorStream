87346a451cb3eb3851574a97a2688191c3ca0fa8
ChengSense <351695627@qq.com>
2023-03-04 15:55:19 +0800

    格式化

--- TensorStream/operators/MaxpoolOperator.h
+++ TensorStream/operators/MaxpoolOperator.h
@@ -19,7 +19,7 @@ class MaxpoolOperator : public TensorOperator {
     int height = (A->shape[0] - kernelSize) / heighs + 1;
     int width = (A->shape[1] - kernelSize) / widths + 1;
     Tenser<None*>* B = zeroNones({height, width});
-    forEach(height, width, kernelSize, kernelSize, 
+    forEach(height, width, kernelSize, kernelSize,
 		[A, B, heighs, widths](int y, int x, int m, int n) {
 			None* inx = A->get<None*>(y * heighs + m, x * widths + n);
 			None* out = B->get<None*>(y, x);
@@ -32,7 +32,7 @@ class MaxpoolOperator : public TensorOperator {
     Tenser<None*>* A = setPadding(getInput<Tenser<None*>*>(0), padding);
     Tenser<None*>* B = getOutput<Tenser<None*>*>();
     int heighs = stride[0], widths = stride[1];
-    forEach(B->shape[1], B->shape[0], kernelSize, kernelSize, 
+    forEach(B->shape[1], B->shape[0], kernelSize, kernelSize,
 		[A, B, heighs, widths](int y, int x, int m, int n) {
 			None* inx = A->get<None*>(y * heighs + m, x * widths + n);
 			None* out = B->get<None*>(y, x);
