eaa756174823648fcd590bd91e349818ccf26c9b
ChengSense <351695627@qq.com>
2023-03-04 15:53:06 +0800

    格式化

--- TensorStream/operators/MaxpoolOperator.h
+++ TensorStream/operators/MaxpoolOperator.h
@@ -19,7 +19,8 @@ class MaxpoolOperator : public TensorOperator {
     int height = (A->shape[0] - kernelSize) / heighs + 1;
     int width = (A->shape[1] - kernelSize) / widths + 1;
     Tenser<None*>* B = zeroNones({height, width});
-    forEach(height, width, kernelSize, kernelSize, [A, B, heighs, widths](int y, int x, int m, int n) {
+    forEach(height, width, kernelSize, kernelSize, 
+		[A, B, heighs, widths](int y, int x, int m, int n) {
 			None* inx = A->get<None*>(y * heighs + m, x * widths + n);
 			None* out = B->get<None*>(y, x);
 			out->value = max(out->value, inx->value);
@@ -31,7 +32,8 @@ class MaxpoolOperator : public TensorOperator {
     Tenser<None*>* A = setPadding(getInput<Tenser<None*>*>(0), padding);
     Tenser<None*>* B = getOutput<Tenser<None*>*>();
     int heighs = stride[0], widths = stride[1];
-    forEach(B->shape[1], B->shape[0], kernelSize, kernelSize, [A, B, heighs, widths](int y, int x, int m, int n) {
+    forEach(B->shape[1], B->shape[0], kernelSize, kernelSize, 
+		[A, B, heighs, widths](int y, int x, int m, int n) {
 			None* inx = A->get<None*>(y * heighs + m, x * widths + n);
 			None* out = B->get<None*>(y, x);
 			inx->setGrad(inx->value == out->value ? out->grad : 0);
