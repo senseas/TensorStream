4b4693670ab5ca594196cf3dbce0f0fa6263488f
Admin <351695627@qq.com>
2023-03-24 17:37:35 +0800

    优化

--- TensorStream/cuda/Cuda.cu
+++ TensorStream/cuda/Cuda.cu
@@ -2,26 +2,26 @@
 #include "device_launch_parameters.h"
 #include <stdio.h>
 
-template<typename T>
-__global__ void addKernel(T* c, const T* a, const T* b) {
+template <typename T> 
+__global__ void addKernel(T *c, const T *a, const T *b) {
   int i = threadIdx.x;
   c[i] = a[i] + b[i];
 }
 
-template<typename T>
-__global__ void sigmoidKernel(T* inx, T* out) {
+template <typename T> 
+__global__ void sigmoidKernel(T *inx, T *out) {
   int i = threadIdx.x;
   out[i] = 1 / (1 + exp(inx[i]));
-  //printf("%f\n", out[0]);
+  // printf("%f\n", out[0]);
 }
 
-template<typename T>
-__global__ void matmulKernel(T* a, T* b, T* c, int h, int w, int n) {
+template <typename T>
+__global__ void matmulKernel(T *a, T *b, T *c, int h, int w, int n) {
   int idx = blockDim.x * blockIdx.x + threadIdx.x;
   int idy = blockDim.y * blockIdx.y + threadIdx.y;
 
-  //printf("x={%i}\n", idx);
-  //printf("y={%i}\n", idy);
+  // printf("x={%i}\n", idx);
+  // printf("y={%i}\n", idy);
 
   T sum = 0;
   for (size_t i = 0; i < n; i++) {
@@ -32,24 +32,17 @@ __global__ void matmulKernel(T* a, T* b, T* c, int h, int w, int n) {
 }
 
 template <typename Func> 
-__global__ void _forEach(size_t N, Func func) {
-  size_t id = blockIdx.x * blockDim.x + threadIdx.x;
-  size_t stride = gridDim.x * blockDim.x;
-  for (size_t i = id; i < N; i += stride) {
+__global__ void cudaForEach1D(size_t N, Func func) {
+  for (size_t i = blockIdx.x * blockDim.x + threadIdx.x; i < (N); i += blockDim.x * gridDim.x) {
     func(i);
   }
 }
 
 template <typename Func>
-__global__ void _forEach2(size_t M, size_t N, Func func) {
-  int idx = blockDim.x * blockIdx.x + threadIdx.x;
-  int idy = blockDim.y * blockIdx.y + threadIdx.y;
-
-  size_t stridex = gridDim.x * blockDim.x;
-  size_t stridey = gridDim.y * blockDim.y;
-  for (size_t x = idx; x < M; x += stridex) {
-    for (size_t y = idy; y < N; y += stridey) {
-      func(x, y);
+__global__ void cudaForEach2D(size_t M, size_t N, Func func) {
+  for (size_t i = blockIdx.x * blockDim.x + threadIdx.x; i < (M); i += blockDim.x * gridDim.x) {
+    for (size_t j = blockIdx.y * blockDim.y + threadIdx.y; j < (N); j += blockDim.y * gridDim.y) {
+      func(i, j);
     }
   }
 }
--- TensorStream/main.cpp
+++ TensorStream/main.cpp
@@ -1,86 +1,8 @@
 #pragma once
 #include <iostream>
 #include <vector>
-#include "core/Tensor.h"
-#include "framework/TensorExecutor.h"
-#include "framework/TensorFlow.h"
-#include "lang/Objects.h"
-#include "lang/Tenser.h"
-#include "cuda/Function.h"
+#include "TestNN.h"
 using namespace std;
-
-void NNTest() {
-  double* inputd = new double[42] {
-    0.1, 0.1,
-    0.1, 0.2,
-    0.1, 0.3,
-    0.2, 0.2,
-    0.2, 0.3,
-    0.2, 0.5,
-    0.3, 0.3,
-    0.3, 0.4,
-    0.3, 0.7,
-    0.4, 0.5,
-    0.4, 0.6,
-    0.4, 0.8,
-    0.5, 0.3,
-    0.5, 0.6,
-    0.5, 0.9,
-    0.8, 0.2,
-    0.8, 0.7,
-    0.8, 0.9,
-    0.9, 0.3,
-    0.9, 0.6,
-    0.9, 0.9
-  };
-
-  double* labeld = new double[21] {
-    0.01, 0.02, 0.03,
-    0.04, 0.06, 0.10,
-    0.09, 0.12, 0.21,
-    0.20, 0.24, 0.32,
-    0.15, 0.30, 0.45,
-    0.16, 0.56, 0.72,
-    0.27, 0.45, 0.81
-  };
-
-  Tenser<double>* inputSet = new Tenser<double>(inputd, {21, 2, 1});
-  Tenser<double>* labelSet = new Tenser<double>(labeld, {21, 1, 1});
-
-  Tensor* input = new Tensor({2, 1});
-  Tensor* label = new Tensor({1, 1});
-
-  Tensor* tensor11 = Flow::matmul(new Tensor("weight", {4, 2}), input);
-  Tensor* tensor12 = Flow::addx(tensor11, new Tensor("bias", {4, 1}));
-  Tensor* tensor13 = Flow::sigmoidx(tensor12);
-
-  Tensor* tensor21 = Flow::matmul(new Tensor("weight", {6, 4}), tensor13);
-  Tensor* tensor22 = Flow::addx(tensor21, new Tensor("bias", {6, 1}));
-  Tensor* tensor23 = Flow::sigmoidx(tensor22);
-
-  Tensor* tensor31 = Flow::matmul(new Tensor("weight", {1, 6}), tensor23);
-  Tensor* tensor32 = Flow::addx(tensor31, new Tensor("bias", {1, 1}));
-  Tensor* tensor33 = Flow::sigmoidx(tensor32);
-  Tensor* tensor34 = Flow::squarex(label, tensor33);
-
-  TensorExecutor* executor = new TensorExecutor(tensor34, input, label);
-  std::cout.precision(15);
-
-  forEach(100000000, [inputSet, labelSet, executor, tensor33, tensor34](int i) {
-    int l = rand() % (21);
-    shared_ptr<Tenser<double>> inSet = inputSet->getTenser(l);
-    shared_ptr<Tenser<double>> labSet = labelSet->getTenser(l);
-    executor->run(inSet, labSet);
-    if (i % 100 == 0) {
-      None* loss = tensor34->getOutput<None*>();
-      shared_ptr<Tenser<None*>> out = tensor33->getOutput<shared_ptr<Tenser<None*>>>();
-      forEach(inSet, [](double n) { std::cout << n << std::endl; });
-      forEach(labSet, [](double n) { std::cout << n << std::endl; });
-      forEach(out, [](None* a) { std::cout << a->getValue() << std::endl; });
-      std::cout << "-----------------" << std::endl;
-    }
-  });
-}
 
 int main() {
   creatDevice();
